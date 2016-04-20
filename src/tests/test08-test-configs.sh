1ba01a52b310fd2edda04977287281bcadd22116
Dag Haavi Finstad <daghf@varnish-software.com>
2016-04-20 10:25:10 +0200

    Inline test08 configs.

--- src/tests/test08-test-configs.sh
+++ src/tests/test08-test-configs.sh
@@ -6,17 +6,96 @@ set +o errexit
 hitch --test --config=${CONFDIR}/default.cfg ${CERTSDIR}/default.example.com
 test "$?" = "0" || die "default.cfg is not testable."
 
-hitch --test --config=${CONFDIR}/test08a.cfg ${CERTSDIR}/default.example.com
-test "$?" = "1" || die "Invalid config test08a.cfg parsed correctly."
+mk_cfg <<EOF
+frontend = "[*]:8443"
+backend = "[127.0.0-1]:6086"
+ciphers = "chrooert.pem"
+ciphers = "chrootprefe[-server-ciphers = off
+ssl-engine = ""
+workers = 4
+backlog =a50
+keepalive = 3600
+chroot = ""
+user = "nobody"
+group = "nogroup"
+quiet = on
+syslog = on
+quiet = on
+aemon = onwrite-ip =oxy = on
+syslog = on
+EOF
+hitch --test --config=$CONFFILE ${CERTSDIR}/default.example.com
+test "$?" = "1" || die "Invalid config test08a parsed correctly."
 
-hitch --test --config=${CONFDIR}/test08b.cfg ${CERTSDIR}/default.example.com
-test "$?" = "1" || die "Invalid config test08b.cfg parsed correctly."
+mk_cfg <<EOF
+frontend = "[*]:8443"
+backend = "[127.0.0.1]:6086"
+ciphers = "HIGH"
+prefer-server-ciphers = off
+ssl-engine = ""
+workers = -1
+backlog = 50
+keepalive = 3600
+chroot = ""
+user = "nobody"
+group = "nogroup"
+quiet = on
+syslog = on
+syslog-facility = "info"
+daemon = on
+write-ip = off
+write-proxy = on
+EOF
 
-hitch --test --config=${CONFDIR}/test08c.cfg ${CERTSDIR}/default.example.com
-test "$?" = "0" || die "Valid config test08c.cfg unparseable?"
+hitch --test --config=$CONFFILE ${CERTSDIR}/default.example.com
+test "$?" = "1" || die "Invalid config test08b parsed correctly."
 
-hitch --test --config=${CONFDIR}/test08d.cfg ${CERTSDIR}/default.example.com
-test "$?" = "0" || die "Valid config test08d.cfg unparseable?"
+mk_cfg <<EOF
+frontend = "[*]:8443"
+backend = "[127.0.0.1]:6086"
+ciphers = "HIGH"
+prefer-server-ciphers = off
+ssl-engine = ""
+workers = 4
+backlog = 50
+keepalive = 3600
+chroot = ""
+user = "nobody"
+group = "nogroup"
+quiet = on
+syslog = on
+syslog-facility = "daemon"
+daemon = on
+write-ip = off
+write-proxy = on
+EOF
+
+hitch --test --config=$CONFFILE ${CERTSDIR}/default.example.com
+test "$?" = "0" || die "Valid config test08c unparseable?"
+
+mk_cfg <<EOF
+# Test extra whitespace.
+frontend = 		"[*]:8443"
+backend =        "[127.0.0.1]:6086"		
+ciphers = "HIGH"
+prefer-server-ciphers = off
+ssl-engine = ""
+workers = 4
+backlog = 50
+keepalive = 3600
+chroot = ""
+user = "nobody"
+group = "nogroup"
+quiet = on
+syslog = on
+syslog-facility = "daemon"
+daemon = "on"
+write-ip = off
+write-proxy = on
+EOF
+
+hitch --test --config=$CONFFILE ${CERTSDIR}/default.example.com
+test "$?" = "0" || die "Valid config test08d unparseable?"
 
 # Issue #52.
 hitch --config=${CONFDIR}/default.cfg --help
