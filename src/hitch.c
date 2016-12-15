15b99459b10a2b7de205c2690c8c81a1180a72d9
Dag Haavi Finstad <daghf@varnish-software.com>
2016-12-15 14:51:27 +0100

    Force SSL_OP_SINGLE_DH_USE to avoid small subgroup attacks on older OpenSSL versions
    Link: https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2016-0701

--- src/hitch.c
+++ src/hitch.c
@@ -859,6 +859,9 @@ make_ctx_fr(const struct cfg_cert_file *cf, const struct frontend *fr,
 #ifdef SSL_OP_NO_COMPRESSION
 	ssloptions |= SSL_OP_NO_COMPRESSION;
 #endif
+#ifdef SSL_OP_SINGLE_DH_USE
+	ssloptions |= SSL_OP_SINGLE_DH_USE;
+#endif
 	if (!(selected_protos & SSLv3_PROTO))
 		ssloptions |= SSL_OP_NO_SSLv3;
 	if (!(selected_protos & TLSv1_0_PROTO))
