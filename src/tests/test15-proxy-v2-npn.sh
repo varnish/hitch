d3fd9310cbfab6ded4bbcb1f601924feb55093b5
Dridi Boukelmoune <dridi.boukelmoune@gmail.com>
2017-10-04 14:43:33 +0200

    Wrap openssl s_client commands in a function
    It takes care of feeding them newlines or checking that options like
    -alpn are supported. Start using it in NPN and ALPN test cases.
    
    About this trick:
    
        ! grep ERROR $DUMPFILE
    
    Instead of using run_cmd we can grep for errors, but this will fail when
    there are no errors. With `set -e` enforced in tests 15 and 16 the lack
    of error would fail the whole test. So instead the exit status must be
    non-zero (thanks to the ! prefix) and when errors occur they are printed
    thanks to the lack of a -q option.

--- src/tests/test15-proxy-v2-npn.sh
+++ src/tests/test15-proxy-v2-npn.sh
@@ -1,37 +1,28 @@
 #!/bin/sh
 
 . hitch_test.sh
-set +o errexit
-
-openssl s_client -help 2>&1 |
-grep -q nextprotoneg ||
-skip "OpenSSL does not support NPN"
 
 BACKENDPORT=`expr $LISTENPORT + 1500`
 
-echo Listen port is $LISTENPORT
-
-hitch $HITCH_ARGS --backend=[127.0.0.1]:$BACKENDPORT "--frontend=[${LISTENADDR}]:$LISTENPORT" \
-	--write-proxy-v2 --alpn-protos="h2,h2-14,http/1.1" \
-	${CERTSDIR}/site1.example.com
-test $? -eq 0 || die "Hitch did not start."
-
-type parse_proxy_v2 || die "Can't find parse_proxy_v2"
 parse_proxy_v2 $BACKENDPORT > $DUMPFILE &
+
+start_hitch \
+	--backend=[127.0.0.1]:$BACKENDPORT \
+	--frontend="[${LISTENADDR}]:$LISTENPORT" \
+	--write-proxy-v2 \
+	--alpn-protos="h2,h2-14,http/1.1" \
+	${CERTSDIR}/site1.example.com
 
 sleep 0.1
 
 # If you have nghttp installed, you can try it instead of openssl s_client:
 # nghttp -v "https://localhost:$LISTENPORT"
 
-echo -e "\n" | openssl s_client -nextprotoneg 'h2-14' -prexit -connect $LISTENADDR:$LISTENPORT > /dev/null
-test $? -eq 0 || die "s_client failed"
+s_client -nextprotoneg 'h2-14' -prexit -connect "$LISTENADDR:$LISTENPORT"
 
-grep -q -c "ERROR" $DUMPFILE
-test $? -ne 0 || die "The utility parse_proxy_v2 gave an ERROR"
+! grep ERROR $DUMPFILE
 
-grep -q -c "h2-14" $DUMPFILE
-test $? -eq 0 || die "No ALPN extension reported"
+run_cmd grep -q h2-14 $DUMPFILE
+run_cmd grep -q "ALPN extension" $DUMPFILE
 
-grep -q -c "ALPN extension" $DUMPFILE
-test $? -eq 0 || die "No ALPN extension reported"
+# XXX: why do we expect ALPN and not NPN here?
