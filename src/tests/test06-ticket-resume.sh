2055e5c0146ccaa77f9953fa9081ae202041b4db
Dridi Boukelmoune <dridi.boukelmoune@gmail.com>
2017-10-05 00:29:17 +0200

    Polish test 06
    Now that tests cd in their own temp directory, we can use that to make
    things a lot more readable.

--- src/tests/test06-ticket-resume.sh
+++ src/tests/test06-ticket-resume.sh
@@ -3,26 +3,15 @@
 # Test resuming a session via a session ticket
 
 . hitch_test.sh
-set +o errexit
 
-SESSFILE=`mktemp`
-rmsess() {
-	rm -f $SESSFILE
-	cleanup
-}
-trap rmsess EXIT
+start_hitch \
+	--backend="[hitch-tls.org]:80" \
+	--frontend="[${LISTENADDR}]:$LISTENPORT" \
+	"${CERTSDIR}/site1.example.com"
 
-hitch $HITCH_ARGS --backend=[hitch-tls.org]:80 "--frontend=[${LISTENADDR}]:$LISTENPORT" \
-	${CERTSDIR}/site1.example.com
-test $? -eq 0 || die "Hitch did not start."
+s_client -sess_out sess_ticket.txt
+s_client -sess_in  sess_ticket.txt >$DUMPFILE
 
-echo -e "\n" | openssl s_client -prexit -sess_out $SESSFILE -connect $LISTENADDR:$LISTENPORT
-test $? -eq 0 || die "s_client failed (1)"
+run_cmd grep Reused, $DUMPFILE
 
-echo -e "\n" | openssl s_client -prexit -sess_in $SESSFILE -connect $LISTENADDR:$LISTENPORT >$DUMPFILE 2>&1
-test $? -eq 0 || die "s_client failed (2)"
-
-grep -q -c "Reused, " $DUMPFILE
-test $? -eq 0 || die "Unable to resume session via session ticket."
-
-runcurl $LISTENADDR $LISTENPORT
+curl_hitch
