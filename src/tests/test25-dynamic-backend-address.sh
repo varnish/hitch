c9c2069fcf3af6d3dfc25ec57a73e4f4579c6e33
Dridi Boukelmoune <dridi.boukelmoune@gmail.com>
2017-11-13 11:26:24 +0100

    Port tests 25, 27 and 28 to the new test helpers
    Whatever happened to test 26, test 27 highlights reload problems with
    regard to relative file names.

--- src/tests/test25-dynamic-backend-address.sh
+++ src/tests/test25-dynamic-backend-address.sh
@@ -1,36 +1,27 @@
 #!/bin/sh
+#
+# Fragile test, double-check the logs on failures.
 
 . hitch_test.sh
-set +o errexit
 
-TMP_HOSTS_FILE=`mktemp`
-echo "hitch_test hitch-tls.org" > $TMP_HOSTS_FILE
-export HOSTALIASES=$TMP_HOSTS_FILE
+echo "hitch_test hitch-tls.org" >hosts
+export HOSTALIASES=$PWD/hosts
 
-mk_cfg <<EOF
+getent hosts hitch_test |
+grep -F hitch-tls.org ||
+skip "HOSTALIASES not supported"
+
+cat >hitch.cfg <<EOF
 pem-file = "${CERTSDIR}/default.example.com"
-frontend = "[$LISTENADDR]:$LISTENPORT"
+frontend = "[localhost]:$LISTENPORT"
 backend = "[hitch_test]:80"
 EOF
 
-hitch $HITCH_ARGS --config=$CONFFILE
-test $? -eq 0 || die "Hitch did not start."
+start_hitch --config=hitch.cfg
+curl_hitch
 
-runcurl $LISTENADDR $LISTENPORT
-curl --max-time 5  --insecure https://$LISTENADDR:$LISTENPORT/ >$DUMPFILE 2>&1
-test $? -eq 0 || die "Old backend address should be available."
-grep  "<title>Hitch TLS proxy" $DUMPFILE
-test $? -eq 0 || die "Old backend served unexpected site"
-
-echo "hitch_test varnish-cache.org" > $TMP_HOSTS_FILE
+echo "hitch_test varnish-cache.org" >hosts
 
 sleep 2
-runcurl $LISTENADDR $LISTENPORT
-
-curl --max-time 5  --insecure https://$LISTENADDR:$LISTENPORT/ >$DUMPFILE 2>&1
-test $? -eq 0 || die "New backend address should be available."
-grep -q -c "We moved the Varnish Project to a new server" $DUMPFILE
-test $? -eq 0 || die "New backend served unexpected site"
-
-rm $TMP_HOSTS_FILE
-
+CURL_STATUS=404
+curl_hitch
