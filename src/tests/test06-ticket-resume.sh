a777d6461f35130a8602342d23c606eb092f8279
Dridi Boukelmoune <dridi.boukelmoune@gmail.com>
2019-01-11 11:51:58 +0100

    Make sure we have sess_ticket.txt in test06

--- src/tests/test06-ticket-resume.sh
+++ src/tests/test06-ticket-resume.sh
@@ -10,6 +10,7 @@ start_hitch \
 	"${CERTSDIR}/site1.example.com"
 
 s_client -sess_out sess_ticket.txt >out.dump
+run_cmd test -f sess_ticket.txt
 s_client -sess_in  sess_ticket.txt >in.dump
 
 run_cmd grep Reused, in.dump
