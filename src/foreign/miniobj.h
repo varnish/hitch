ad7262cc09e21f6b23414f5b7717e6bf5b525dfd
Dag Haavi Finstad <daghf@varnish-software.com>
2024-12-09 13:40:33 +0100

    foreign/miniobj.h: Update from upstream

--- src/foreign/miniobj.h
+++ src/foreign/miniobj.h
@@ -5,22 +5,38 @@
  *
  */
 
+#define ZERO_OBJ(to, sz)						\
+	do {								\
+		void *(*volatile z_obj)(void *, int, size_t) = memset;	\
+		(void)z_obj(to, 0, sz);					\
+	} while (0)
+
 #define INIT_OBJ(to, type_magic)					\
 	do {								\
-		(void)memset(to, 0, sizeof *to);			\
+		(void)memset(to, 0, sizeof *(to));			\
 		(to)->magic = (type_magic);				\
 	} while (0)
 
 #define ALLOC_OBJ(to, type_magic)					\
 	do {								\
-		(to) = calloc(sizeof *(to), 1);				\
+		(to) = calloc(1, sizeof *(to));				\
 		if ((to) != NULL)					\
 			(to)->magic = (type_magic);			\
 	} while (0)
 
+#define ALLOC_OBJ_EXTRA(to, extra_size, type_magic)			\
+	do {								\
+		(to) = calloc(1, sizeof(*(to)) + (extra_size));		\
+		if ((to) != NULL)					\
+			(to)->magic = (type_magic);			\
+	} while (0)
+
+#define ALLOC_FLEX_OBJ(to, fld, len, type_magic)			\
+	ALLOC_OBJ_EXTRA(to, (len) * sizeof *((to)->fld), (type_magic))
+
 #define FREE_OBJ(to)							\
 	do {								\
-		(to)->magic = (0);					\
+		ZERO_OBJ(&(to)->magic, sizeof (to)->magic);		\
 		free(to);						\
 		to = NULL;						\
 	} while (0)
@@ -30,19 +46,19 @@
 
 #define CHECK_OBJ(ptr, type_magic)					\
 	do {								\
-		assert((ptr)->magic == type_magic);			\
+		assert((ptr)->magic == type_magic);		\
 	} while (0)
 
 #define CHECK_OBJ_NOTNULL(ptr, type_magic)				\
 	do {								\
 		assert((ptr) != NULL);					\
-		assert((ptr)->magic == type_magic);			\
+		assert((ptr)->magic == type_magic);		\
 	} while (0)
 
 #define CHECK_OBJ_ORNULL(ptr, type_magic)				\
 	do {								\
 		if ((ptr) != NULL)					\
-			assert((ptr)->magic == type_magic);		\
+			assert((ptr)->magic == type_magic);	\
 	} while (0)
 
 #define CAST_OBJ(to, from, type_magic)					\
@@ -55,16 +71,24 @@
 #define CAST_OBJ_NOTNULL(to, from, type_magic)				\
 	do {								\
 		(to) = (from);						\
-		assert((to) != NULL);					\
+		AN((to));						\
 		CHECK_OBJ((to), (type_magic));				\
+	} while (0)
+
+#define TAKE_OBJ_NOTNULL(to, pfrom, type_magic)				\
+	do {								\
+		AN((pfrom));						\
+		(to) = *(pfrom);					\
+		*(pfrom) = NULL;					\
+		CHECK_OBJ_NOTNULL((to), (type_magic));			\
 	} while (0)
 
 #define REPLACE(ptr, val)						\
 	do {								\
-		if ((ptr) != NULL)					\
-			free(ptr);					\
-		if ((val) != NULL) {					\
-			ptr = strdup(val);				\
+		const char *_vreplace = (val);				\
+		free(ptr);						\
+		if (_vreplace != NULL) {				\
+			ptr = strdup(_vreplace);			\
 			AN((ptr));					\
 		} else {						\
 			ptr = NULL;					\
